ca60f4ab1864c11ba103adc02d2813b9f77cb0e2
ASDAFF <asdaff.asad@yandex.ru>
2020-01-31 01:30:26 +0500

    up

--- install/lang/ru/install/index.php
+++ install/lang/ru/install/index.php
@@ -1,12 +1,12 @@
 <?
 /**
- * Copyright (c) 2019 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
+ * Copyright (c) 2020 Created By/Edited By ASDAFF asdaff.asad@yandex.ru
  */
 
 $MESS["EASY_FORM_PARTNER_NAME"] = "ASDAFF";
 $MESS["EASY_FORM_PARTNER_URI"] = "https://asdaff.github.io";
 $MESS["EASY_FORM_MODULE_NAME"] = "Настраиваемая форма обратной связи";
-$MESS["EASY_FORM_MODULE_DESCRIPTION"] = "Модуль представляет собой удобное решение для реализации форм обратной связи";
+$MESS["EASY_FORM_MODULE_DESCRIPTION"] = "Модуль для реализации форм обратной связи";
 $MESS["EASY_FORM_INSTALL_TITLE"] = "Установка модуля \"{$MESS["EASY_FORM_MODULE_NAME"] }\"";
 $MESS["EASY_FORM_UNINSTALL_ERROR"] = "Обнаружены ошибки при удалении модуля";
 $MESS["EASY_FORM_OPTION_MESSAGE_TEXT_DEFAULT"] = 'Нажимая на кнопку "#BUTTON#", вы даете согласие на обработку <a target="_blank" href="#">персональных данных</a>';
